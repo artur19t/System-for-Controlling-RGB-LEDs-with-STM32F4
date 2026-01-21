aafb1acc63db3d45db4b48b3141c2d675273b08a
Artur19t <artur19t@yandex.ru>
2026-01-21 21:06:08 +0300

    Init handlers for button

--- User/Src/hw_init.c
+++ User/Src/hw_init.c
@@ -1,7 +1,7 @@
 #include "hw_init.h"
 
 // --- GPIO ---
-
+//PB 0,1,2
 // --- PB0, Button for bed led ---
 void GPIO_Button_Bed_Init(void)
 {
@@ -40,4 +40,33 @@ void GPIO_Button_Room_Init(void)
   GPIO_Usr_Struct.Speed = LL_GPIO_SPEED_FREQ_LOW;
   LL_GPIO_Init(GPIOB, &GPIO_Usr_Struct);
 }
+
+// --- Interrupts ----
+void IT_BUTTONS_Init(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_EXTI_InitTypeDef IT_But_Init = {0};
+  IT_But_Init.LineCommand = ENABLE;
+  IT_But_Init.Line_0_31 = LL_EXTI_LINE_0 | LL_EXTI_LINE_1 | LL_EXTI_LINE_2;
+  IT_But_Init.Mode = LL_EXTI_MODE_IT;
+  IT_But_Init.Trigger = LL_EXTI_TRIGGER_RISING;
+  LL_EXTI_Init(&IT_But_Init);
+  
+  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE0);
+  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE1);
+  LL_SYSCFG_SetEXTISource(LL_SYSCFG_EXTI_PORTB, LL_SYSCFG_EXTI_LINE2);
+  
+  LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_0);
+  NVIC_SetPriority(EXTI0_IRQn, 0);
+  NVIC_EnableIRQ(EXTI0_IRQn);
+  
+  LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_1);
+  NVIC_SetPriority(EXTI1_IRQn, 0);
+  NVIC_EnableIRQ(EXTI1_IRQn);
+  
+  LL_EXTI_EnableIT_0_31(LL_EXTI_LINE_2);
+  NVIC_SetPriority(EXTI2_IRQn, 0);
+  NVIC_EnableIRQ(EXTI2_IRQn);
+}
+
 // --- USART ---
