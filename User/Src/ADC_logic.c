dfa4bb6602f0e8e4bb6a7ecfa615c06d1c0361ec
Artur19t <artur19t@yandex.ru>
2026-01-26 16:04:55 +0300

    Add color logic

--- User/Src/ADC_logic.c
+++ User/Src/ADC_logic.c
@@ -17,6 +17,7 @@ void encoder_task(void)
   if (abs(adc-adc_prev)>20)
   {
     brightness = map(adc, 0, 4095, 0, 1000);
+    color = map(adc, 0, 4095, 0, 360);
     adc_prev = adc;
   }
 }
@@ -36,4 +37,33 @@ static uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_m
   return (x - in_min) * (out_max - out_min)
          / (in_max - in_min)
          + out_min;
+}
+
+typedef struct {
+  uint16_t r;
+  uint16_t g;
+  uint16_t b;
+} rgb_t;
+
+rgb_t HSV_to_RGB(uint16_t h, uint8_t s, uint16_t v)
+{
+  rgb_t out = {0};
+
+  uint8_t region = h / 60;
+  uint16_t remainder = (h - region * 60) * 255 / 60;
+
+  uint16_t p = (v * (255 - s)) / 255;
+  uint16_t q = (v * (255 - (s * remainder) / 255)) / 255;
+  uint16_t t = (v * (255 - (s * (255 - remainder)) / 255)) / 255;
+
+  switch (region) {
+    case 0: out = (rgb_t){v, t, p}; break;
+    case 1: out = (rgb_t){q, v, p}; break;
+    case 2: out = (rgb_t){p, v, t}; break;
+    case 3: out = (rgb_t){p, q, v}; break;
+    case 4: out = (rgb_t){t, p, v}; break;
+    default: out = (rgb_t){v, p, q}; break;
+  }
+
+  return out;
 }
