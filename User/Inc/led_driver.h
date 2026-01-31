c4aaf4fafe2de5548534cdbd92ae8afb65b08510
Artur19t <artur19t@yandex.ru>
2026-01-31 09:18:31 +0300

    Add RGB controller

--- User/Inc/led_driver.h
+++ User/Inc/led_driver.h
@@ -22,5 +22,10 @@ typedef struct {
 void encoder_read_delta(encoder_st *encoder_user);
 void LIGHT_state_handler(but_struct *btn, light_st *light);
 void LIGHT_State_Init(light_st *light);
+void LIGHT_CalcRGB(const light_st *light, uint16_t *pwm_r, uint16_t *pwm_g, uint16_t *pwm_b);
+static void tone_white(uint16_t tone, uint16_t *r, uint16_t *g, uint16_t *b);
+static void hue_to_rgb(uint16_t hue, uint16_t *r, uint16_t *g, uint16_t *b);
+int32_t clamp (uint32_t v, uint32_t min, uint32_t max);
+int32_t lerp(int32_t a, int32_t b, int32_t t, int32_t t_max);
 
 #endif
--- User/Src/led_driver.c
+++ User/Src/led_driver.c
@@ -61,6 +61,8 @@ void LIGHT_state_handler(but_struct *btn, light_st *light)
     switch (btn->time_p)
     {
       case SHORT_PRESSED:
+        light->turn_on = !light->turn_on;
+        btn->released_event = 0;
         break;
       case LONG_PRESSED:
         if (!light->adjusted)
@@ -70,20 +72,12 @@ void LIGHT_state_handler(but_struct *btn, light_st *light)
           encoder_st_tone.delta = 0;
         }
         light->adjusted = true;
-        tmp = light->bright += encoder_st_bright.delta;
-        if (tmp < 0) tmp = 0;
-        if (tmp > 1000) tmp = 1000;
-        light->bright = tmp;
         
-        tmp = light->color += encoder_st_color.delta;
-        if (tmp < 0) tmp = 0;
-        if (tmp > 1000) tmp = 1000;
-        light->color = tmp;
+        light->bright = clamp(light->bright + encoder_st_bright.delta, 0, 1000);
         
-        tmp = light->tone += encoder_st_tone.delta;
-        if (tmp < 0) tmp = 0;
-        if (tmp > 1000) tmp = 1000;
-        light->tone = tmp;
+        light->color = clamp(light->color + encoder_st_color.delta, 0, 1000);
+        
+        light->tone = clamp(light->tone + encoder_st_tone.delta, 0, 1000);
         
         encoder_st_bright.delta = 0;
         encoder_st_color.delta = 0;
@@ -92,12 +86,12 @@ void LIGHT_state_handler(but_struct *btn, light_st *light)
       case LONGLONG_PRESSED:
         btn->time_p = NOT_PRESSED;
         light->adjusted = false;
+        btn->released_event = 0;
         break;
       default:
         break;
     }
   }
-  btn->released_event = 0;
 }
 
 void LIGHT_State_Init(light_st *light)
@@ -107,4 +101,114 @@ void LIGHT_State_Init(light_st *light)
   light->tone = 650;
   light->turn_on = false;
   light->adjusted = false;
+}
+
+int32_t clamp (uint32_t v, uint32_t min, uint32_t max)
+{
+  if (v < min) return min;
+  if (v > max) return max;
+  return v;
+}
+
+int32_t lerp(int32_t a, int32_t b, int32_t t, int32_t t_max)
+{
+    return a + (b - a) * t / t_max;
+}
+
+void LIGHT_CalcRGB(const light_st *light, uint16_t *pwm_r, uint16_t *pwm_g, uint16_t *pwm_b)
+{
+  uint16_t r, g, b;
+
+  if (!light->turn_on || light->bright == 0)
+  {
+    *pwm_r = *pwm_g = *pwm_b = 0;
+    return;
+  }
+
+  if (light->color == 0)
+  {
+    tone_white(light->tone, &r, &g, &b);
+  }
+  else
+  {
+    hue_to_rgb(light->color, &r, &g, &b);
+  }
+
+  r = r * light->bright / 1000;
+  g = g * light->bright / 1000;
+  b = b * light->bright / 1000;
+
+  *pwm_r = r;
+  *pwm_g = g;
+  *pwm_b = b;
+
+  if (*pwm_r > 1000) *pwm_r = 1000;
+  if (*pwm_g > 1000) *pwm_g = 1000;
+  if (*pwm_b > 1000) *pwm_b = 1000;
+}
+
+static void tone_white(uint16_t tone, uint16_t *r, uint16_t *g, uint16_t *b)
+{
+  if (tone <= 500)
+  {
+    *r = 1000;
+    *g = 800 + (tone * 200) / 500;
+    *b = 400 + (tone * 600) / 500;
+  }
+  else
+  {
+    uint16_t t = tone - 500;
+    *r = 1000 - (t * 600) / 500;
+    *g = 1000 - (t * 100) / 500;
+    *b = 1000;
+  }
+}
+
+static void hue_to_rgb(uint16_t hue, uint16_t *r, uint16_t *g, uint16_t *b)
+{
+  // hue: 0..1000
+  uint32_t h = hue * 6;   // 0..6000
+  uint32_t sector = h / 1000;   // 0..5
+  uint32_t pos = h % 1000;      // 0..999
+
+  uint32_t inv = 1000 - pos;
+
+  switch (sector)
+  {
+    case 0: // R -> Y
+      *r = 1000;
+      *g = pos;
+      *b = 0;
+      break;
+
+    case 1: // Y -> G
+      *r = inv;
+      *g = 1000;
+      *b = 0;
+      break;
+
+    case 2: // G -> C
+      *r = 0;
+      *g = 1000;
+      *b = pos;
+      break;
+
+    case 3: // C -> B
+      *r = 0;
+      *g = inv;
+      *b = 1000;
+      break;
+
+    case 4: // B -> M
+      *r = pos;
+      *g = 0;
+      *b = 1000;
+      break;
+
+    default: // M -> R
+      *r = 1000;
+      *g = 0;
+      *b = inv;
+      break;
+  }
 }
